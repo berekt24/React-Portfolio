713aac994d626695dcbe8204f17a4dae759e9133
berekt24 <bashenafi35@gmail.com>
2023-01-14 14:51:02 -0500

    lined up project cards

--- student-portfolio/src/components/pages/Projects.js
+++ student-portfolio/src/components/pages/Projects.js
@@ -5,32 +5,41 @@ export default function Projects() {
     const projectsArray = [
         {
             title:"More Than A Game",
-            desc:"",
+            desc:"This app allows you to search for local events around the college football game you are attending. ",
             img: project1,
-            github:"",
-            deployed:""
+            github:"https://github.com/mfrabott/More-Than-A-Game",
+            deployed:"https://safe-lake-28734.herokuapp.com/"
         },
         {
             title:"SneakersPeak",
-            desc:"project 2",
+            desc:"This app is a place to view the latest sneakers and comment your thoughts on each one. It also has a page with links to other fashion websites similiar to this one.",
             img: project2,
             github:"",
             deployed:""
         },
     ] 
   return (
-    <div>
+    
+      <div>
       <h1 class="projects-header">Projects</h1>
+      <div 
+      class="projects-container"
+      style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center'}}>
      {projectsArray.map(project =>(
         <div className="card" style= {{width: "18rem"}}>
         <img src={project.img} className="card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{project.title}</h5>
           <p className="card-text">{project.desc}</p>
+          <div style={{margin: '2%'}}>
           <a href={project.github} className="btn btn-primary">Github Repo</a>
+          </div>
+          <a href={project.deployed} className="btn btn-primary">Deployed Link</a>
+          
         </div>
       </div>
      ))}
+     </div>
     </div>
   );
 }
